2365d2cbffce9d381314f0c3429a8a7773f5da8e
Peiqin Zhao <peiqinzhao@google.com>
2019-03-05 00:00:59 -0800

    update data model for Pi

--- FirestoreIoT/Pi.swift
+++ FirestoreIoT/Pi.swift
@@ -9,5 +9,26 @@
 import UIKit
 
 class Pi {
+    //MARK: Properties
+    var errorRate: Double
+    var samplingRate: Double
+    var sampledValue: Double
+    var name: String
     
+    
+    init?(errorRate: Double, samplingRate: Double, sampledValue: Double, name: String) {
+        
+        guard samplingRate > 0 && samplingRate < 100 else {
+            return nil
+        }
+        
+        guard errorRate > 0 && errorRate < 100 else {
+            return nil
+        }
+        
+        self.errorRate = errorRate
+        self.samplingRate = samplingRate
+        self.sampledValue = sampledValue
+        self.name = name
+    }
 }
